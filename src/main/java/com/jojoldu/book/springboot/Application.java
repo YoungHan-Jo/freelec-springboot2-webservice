eba4976003fce29c8fbb467fecd4907cbd917f7e
JYH <verin4494@gmail.com>
2021-12-06 23:44:55 +0900

    junit test

---
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application { // 프로젝트의 메인 클래스
+    public static void main(String[] args) {
+        // 내장 WAS 실행
+        SpringApplication.run(Application.class, args);
+    }
+}
